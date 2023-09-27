ccadae266e4614df3fbddf9ea1c4e414d2768302
camilobeta11 <camilobeta11@hotmail.com>
2023-09-27 16:07:45 -0500

    add update product

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -5,29 +5,48 @@ const router = express.Router();
 
 // Get all products
 router.get('/', async (req: Request, res: Response) => {
-    try {
-      const products = await Product.find();
-      res.json(products);
-    } catch (error) {
-      if (error instanceof Error) {
-        res.status(500).json({ message: error.message });
-      }
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
     }
-  });
+  }
+});
 
-  // Create a new product
-  router.post('/', async (req: Request, res: Response) => {
-    const product = new Product(req.body);
+// Create a new product
+router.post('/', async (req: Request, res: Response) => {
+  const product = new Product(req.body);
 
-    try {
-      const newProduct = await product.save();
-      res.status(201).json(newProduct);
-    } catch (error) {
-      if (error instanceof Error) {
-        res.status(500).json({ message: error.message });
-      }
+  try {
+    const newProduct = await product.save();
+    res.status(201).json(newProduct);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
     }
-  });
+  }
+});
+
+// Update a product
+router.put('/:id', async (req: Request, res: Response) => {
+  try {
+    const productId = req.params.id;
+    const updatedProductData = req.body;
+    const updatedProduct = await Product.findByIdAndUpdate(productId, updatedProductData, {
+      new: true
+    });
+    if (!updatedProduct) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    res.json(updatedProduct);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+});
 
 
 
